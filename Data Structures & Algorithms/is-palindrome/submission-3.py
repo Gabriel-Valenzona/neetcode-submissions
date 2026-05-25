9899c9cdafd7be8fe8a2bfa9723cb6f200b23fb3
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-05-24 22:35:17 -0500

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # space complexity: o(1) regardless of string size, nothing is stored
+        # time complexity: o(n) - at worse case, we iterate through the size of the string so the algorithm grows linearly 
+        l = 0
+        r = len(s) - 1
+
+        while l < r: # when left pointer intersects with right then we know that entire string has been processed
+            while l < r and not self.isalnum(s[l]): # move left pointer toward center until it reaches a alphanumeric
+                l += 1
+            while r > l and not self.isalnum(s[r]): # move right pointer toward center until it reaches a alphanumeric
+                r += 1
+            if s[l].lower() != s[r].lower():
+                return False
+        
+        return True
+
+        
+
+
+
+
+                
