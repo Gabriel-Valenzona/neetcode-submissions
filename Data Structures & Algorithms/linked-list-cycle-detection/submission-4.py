7e3f6ecff9ba0bdbb4416c11b6e6c9b962b3178d
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-06-09 04:23:45 -0500

    Add: linked-list-cycle-detection - submission-4

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-4.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # init fast and slow pointers - floyd's algorithm
+        slow, fast = head, head
+        # idea is: if there is a cycle, then eventually the fast pointer will be at the same node 
+        # as the slow pointer on an iteration
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            fast = fast.next.next
+            slow = slow.next
+        
+        return False
+
+
+
+        
+
+        
+
