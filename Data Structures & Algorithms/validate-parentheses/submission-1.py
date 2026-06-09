908f91080c17de0f660fc6afc55e38f81b708e9a
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-06-08 23:58:35 -0500

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # init stack
+        stack = []
+
+        # use hashmap to fetch matching opening bracket
+        matching_pairs = {']': '[', ')': '(', '}': '{'}
+
+        for c in s:
+            if c in matching_pairs:
+                if stack and stack[-1] == matching_pairs[c]: # DID NOT GET
+                    stack.pop() # DID NOT GET
+                else: # DID NOT GET
+                    return False # DID NOT GET
+            else:
+                stack.append(c)
+        
+        return True if not stack else False
+        # worse case - o(n) time complexity because we have to iterate index by index in string
+        # iterate through string, if open bracket is found then append its corresponding closed bracked to stack
+
+        # once 
