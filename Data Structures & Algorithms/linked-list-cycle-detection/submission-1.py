af7eeab2d1083dd15e9ddf115c1a978c87d367dd
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-06-09 04:06:05 -0500

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # use hashset time and space complexity o(n) to populate the set according to size of linkedlist
+        seen = set()
+        curr = head
+        # if node value is seen in hashset, take advantage of the o(1) access time, then return True
+        while curr:
+            if curr.val in seen:
+                return True
+            seen.add(curr.val)
+        
+        return False
+
+        
+
