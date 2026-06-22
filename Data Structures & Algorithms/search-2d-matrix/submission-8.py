c0f8cad1c19a701c8a232660695ea79fc067c261
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-06-21 22:32:21 -0500

    Add: search-2d-matrix - submission-8

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-8.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for r in matrix:
+            for c in matrix[r]:
+                if target == matrix[r][c]:
+                    return True
+
+        return False
