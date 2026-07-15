68b5e9662d49150ebe7faa75c8f910593214cefe
Gabriel-Valenzona <146038459+Gabriel-Valenzona@users.noreply.github.com>
2026-07-14 22:24:14 -0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # init stack - contains unclosed parenthesis
+        stack = []
+        p_pairs = {'}': '{', ']':'[', ')':'('}
+        # iterate through string - o(n) best time and space complexity
+        if not s:
+            return True
+
+        for c in s:
+            # condition 1 - if closed bracket, check if top of stack matches pair
+            if c in p_pairs:
+                # if not equal, return False
+                if stack[-1] != p_pairs[c] and stack:
+                    return False
+                stack.pop()
+            # condition 2 - if open bracket, append to stack                
+            else:
+                stack.append(c)
+
+        # T if empty, F if populated
+        return not stack
